f47b2900a53a01f4d39f49e516079101c357cc99
Nathan Sosnovske <nsosnov@microsoft.com>
2018-11-14 14:15:24 -0800

    Fixes

---
+++ set-artifact-name.ps1
@@ -0,0 +1,7 @@
+$vstsDrop = (Get-ChildItem $env:BUILD_ARTIFACTSTAGINGDIRECTORY -Recurse -Filter VSTSDrop.json)[0].FullName
+
+$obj = get-content $vstsDrop | convertfrom-json
+
+$url = $obj['VstsDropBuildArtifact']['VstsDropUrl']
+
+Write-Host $url
